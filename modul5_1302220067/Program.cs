42ba7b59dcd86e8f4ef607825256ba15a28fe90e
Indra Mahesa <indramahesa128@gmail.com>
2024-03-18 07:10:19 +0700

    menambahkan class SimpleDataBase

--- modul5_1302220067/Program.cs
+++ modul5_1302220067/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var database = new SimpleDataBase<int>();
+        database.AddNewData(12);  
+        database.AddNewData(02);
+        database.AddNewData(22);
+
+        database.PrintAllData();
+    }
+}
+
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase() 
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data {0} berisi: {1}, yang disimpan pada waktu UTC: {2}", i + 1, storedData[i], inputDates[i]);
+        }
+    }
+}
